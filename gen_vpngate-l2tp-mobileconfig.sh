37179527ce81481070011c57153be331d7eae6dc
MiCHiLU <djmchl@gmail.com>
2013-11-26 09:48:59 +0800

    update

--- gen_vpngate-l2tp-mobileconfig.sh
+++ gen_vpngate-l2tp-mobileconfig.sh
@@ -1,9 +1,41 @@
 #!/usr/bin/env bash
 
+function debug {
+if [ 1 -eq 0 ]; then
+  declare -p $1
+fi
+}
+
+case $1 in
+  "" ) hostname="http://www.vpngate.net" ;;
+   * ) hostname=$1 ;;
+esac
+
 name=VPN-Gate-L2TP-`date +%Y-%m-%d`
 output=${name}.mobileconfig
+CONNECT_TIMEOUT=10
 
-read -rd '' input
+VPNGATE_MIRRORS=`curl -f -s ${hostname}/en/sites.aspx|grep "^<li><strong><span style='font-size: medium'><a href='"|sed -e "s/<li><strong><span style='font-size: medium'><a href='\([^']*\)\/en\/.*/\1/g"`
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+  exit ${PIPESTATUS[0]}
+fi
+
+for mirror in ${VPNGATE_MIRRORS}; do
+VPNGATE_IP_L2TP=`curl -f -s --connect-timeout ${CONNECT_TIMEOUT} ${mirror}/en/ -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Cache-Control: max-age=0' --compressed|grep "^<td class='vg_table_row_[01]' style='text-align: center;'><img src='../images/flags/"|grep L2TP/IPsec|sed -e 's/.*>\([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\)<.*/\1/g'`
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+  continue
+fi
+if [ ${#VPNGATE_IP_L2TP[@]} -gt 0 ]; then
+  if [ "${VPNGATE_IP_L2TP}" != "" ]; then
+    input=${VPNGATE_IP_L2TP}
+    break
+  fi
+fi
+done
+
+if [ ${#input[@]} -eq 0 ]; then
+  exit 1
+fi
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
